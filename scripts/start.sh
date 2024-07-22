c618bb6b19df3d2bca0687b630793b9ca0c53957
gbeea1004 <jvckgh@gmail.com>
2024-07-22 13:45:11 +0900

    환경변수 github action 로직 제거

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
 PROJECT_ROOT="/home/ubuntu/app/deploy"
 JAR_FILE="$PROJECT_ROOT/alarm-mon.jar"
-ENV_FILE="$PROJECT_ROOT/.env"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -12,16 +11,9 @@ TIME_NOW=$(date +%c)
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
-# 환경 변수 파일 로드
-if [ -f "$ENV_FILE" ]; then
-  export $(cat $ENV_FILE | xargs)
-else
-  echo "$TIME_NOW > $ENV_FILE 파일이 없습니다." >> $DEPLOY_LOG
-fi
-
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -DTEAMS_CROSSFIT_NOTI_URL=${TEAMS_CROSSFIT_NOTI_URL} -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
