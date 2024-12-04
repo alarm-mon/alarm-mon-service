76d78de61129c741f3ae2f3cd84e687b8c23af49
gbeea1004 <jvckgh@gmail.com>
2024-12-04 15:56:43 +0900

    fix: 문법 오류 수정

--- src/main/java/geonhee/alarmmon/external/teams/controller/TeamsNotificationController.java
+++ src/main/java/geonhee/alarmmon/external/teams/controller/TeamsNotificationController.java
@@ -6,10 +6,12 @@ import geonhee.alarmmon.external.teams.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/teams")
+@RequestMapping("/teams")
+@RestController
 public class TeamsNotificationController {
 
     private final NotificationService notificationService;
